25f6dd04d38a519034ef061d1103e51adba0b5cb
Maurice Bonemeijer <maurice.bonemeijer@gmail.com>
2017-10-07 16:12:14 +0200

    replaced working name with SMARTDUMP

--- src/Node/Type/NodeType.php
+++ src/Node/Type/NodeType.php
@@ -2,7 +2,7 @@
 /**
  * MIT License
  *
- * Copyright (c) 2008-2017 OMNIDUMP
+ * Copyright (c) 2008-2017 SMARTDUMP
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
