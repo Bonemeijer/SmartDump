e23c7b4ed5b90051bb73194b42cecb40453a5aec
Maurice Bonemeijer <maurice.bonemeijer@gmail.com>
2017-10-07 15:45:45 +0200

    added initial PlainTextFormatter

---
+++ src/Formatter/Text/PlainTextFormatter.php
@@ -0,0 +1,153 @@
+<?php
+/**
+ * MIT License
+ *
+ * Copyright (c) 2008-2017 OMNIDUMP
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace SmartDump\Formatter\Text;
+
+use SmartDump\Formatter\StringFormatterInterface;
+use SmartDump\Node\NodeInterface;
+
+/**
+ * Class PlainTextFormatter
+ *
+ * @package    SmartDump
+ * @subpackage Formatter
+ */
+class PlainTextFormatter implements StringFormatterInterface
+{
+    const DEFAULT_MAX_DEPTH          = 5;
+    const DEFAULT_INDENTATION_STRING = '    ';
+
+    /** @var int */
+    protected $maxDepth;
+
+    /** @var string */
+    protected $indentationString;
+
+    /**
+     * Constructor
+     *
+     * @param int    $maxDepth
+     * @param string $indentationString
+     */
+    public function __construct(
+        $maxDepth = self::DEFAULT_MAX_DEPTH,
+        $indentationString = self::DEFAULT_INDENTATION_STRING
+    ) {
+        $this->maxDepth          = $maxDepth;
+        $this->indentationString = $indentationString;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function format(NodeInterface $node)
+    {
+        $output = $this->formatNode($node);
+
+        // always end with a newline
+        $output .= PHP_EOL;
+
+        return $output;
+    }
+
+    /**
+     * Format a node to string
+     *
+     * @param NodeInterface $node
+     * @param int           $currentDepth
+     * @return string
+     */
+    protected function formatNode(NodeInterface $node, $currentDepth = 0)
+    {
+        if ($node->isAggregate()) {
+            return $this->formatAggregateNode($node, $currentDepth);
+        }
+
+        return $this->formatScalarNode($node);
+    }
+
+    /**
+     * Format a scalar node to string
+     *
+     * @param NodeInterface $node
+     * @return string
+     */
+    protected function formatScalarNode(NodeInterface $node)
+    {
+        return $node->getStringValue();
+    }
+
+    /**
+     * Foramt an aggregate node to string
+     *
+     * @param NodeInterface $node
+     * @param int           $currentDepth
+     * @return string
+     */
+    protected function formatAggregateNode(NodeInterface $node, $currentDepth = 0)
+    {
+        // check for max depth setting
+        if ($currentDepth >= $this->maxDepth) {
+            return $node->getStringValue() . ' *MAX_DEPTH*';
+        }
+
+        $output = '';
+        $output .= $node->getStringValue() . ' (' . PHP_EOL;
+
+        foreach ($node->getChildren() as $key => $childNode) {
+            $childName       = $childNode->getName();
+            $childVisibility = $childNode->getVisibility();
+
+            if (null !== $childVisibility) {
+                $childName .= ':' . $childVisibility;
+            }
+
+            if ($childNode->isStatic()) {
+                $childName .= ':static';
+            }
+
+            $output .= $this->renderIndentation($currentDepth + 1);
+            $output .= sprintf('[%s]', $childName);
+            $output .= ' => ';
+            $output .= $this->formatNode($childNode, $currentDepth + 1);
+            $output .= PHP_EOL;
+        }
+
+        $output .= $this->renderIndentation($currentDepth) . ')';
+
+        return $output;
+    }
+
+    /**
+     * Render indentation
+     *
+     * @param int $level
+     * @return string
+     */
+    protected function renderIndentation($level)
+    {
+        return str_repeat($this->indentationString, $level);
+    }
+}
