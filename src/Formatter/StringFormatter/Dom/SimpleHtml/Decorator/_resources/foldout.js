20d59a37b5cf27ce6dbcdabc6b96647ad6434cdb
Maurice Bonemeijer <maurice.bonemeijer@gmail.com>
2017-10-27 16:13:34 +0200

    added "aggregate-node-foldout" class to aggregate node with foldout decorator

--- src/Formatter/StringFormatter/Dom/SimpleHtml/Decorator/_resources/foldout.js
+++ src/Formatter/StringFormatter/Dom/SimpleHtml/Decorator/_resources/foldout.js
@@ -1,6 +1,7 @@
 //<![CDATA[
 var nodeList = document.querySelectorAll('.aggregate-node .aggregate-node .aggregate-node-name');
 for (var i = 0, length = nodeList.length; i < length; i++) {
+    nodeList[i].parentNode.classList.add('aggregate-node-foldout');
     nodeList[i].addEventListener('click', function(){
         var node = this;
         var nextNode = node.nextSibling;
