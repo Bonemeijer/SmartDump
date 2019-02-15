47515786062a1659cf73226fc6b0721a69a6e73f
Maurice Bonemeijer <Bonemeijer@users.noreply.github.com>
2019-02-15 10:48:10 +0100

    fixed an issue where multiple foldout scripts on the same would break each other

--- src/Formatter/StringFormatter/Dom/SimpleHtml/Decorator/_resources/foldout.js
+++ src/Formatter/StringFormatter/Dom/SimpleHtml/Decorator/_resources/foldout.js
@@ -1,8 +1,6 @@
 //<![CDATA[
-var nodeList = document.querySelectorAll('.aggregate-node .aggregate-node .aggregate-node-name');
-for (var i = 0, length = nodeList.length; i < length; i++) {
-    nodeList[i].parentNode.classList.add('aggregate-node-foldout');
-    nodeList[i].addEventListener('click', function(){
+if (typeof clickEventListener === 'undefined') {
+    var clickEventListener = function(){
         var node = this;
         var nextNode = node.nextSibling;
 
@@ -11,6 +9,13 @@ for (var i = 0, length = nodeList.length; i < length; i++) {
         } else {
             nextNode.classList.add('open');
         }
-    });
+    };
+}
+
+var nodeList = document.querySelectorAll('.aggregate-node .aggregate-node .aggregate-node-name');
+for (var i = 0, length = nodeList.length; i < length; i++) {
+    nodeList[i].parentNode.classList.add('aggregate-node-foldout');
+    nodeList[i].removeEventListener('click', clickEventListener, true);
+    nodeList[i].addEventListener('click', clickEventListener, true);
 }
 //]]>
